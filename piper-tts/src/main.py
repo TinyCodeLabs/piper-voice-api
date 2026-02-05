1972f2bcd81f02d9203123bd056492ce8aa404e3
Tim Gabrikowski <tim@gabrikowski.de>
2026-02-05 21:54:21 +0100

    Remove anything not working

--- piper-tts/src/main.py
+++ piper-tts/src/main.py
@@ -1,26 +1,44 @@
 #!/usr/bin/env python3
-
+import os
 import wave
 from piper import PiperVoice, SynthesisConfig
 
-VOICE_PATH = "piper-tts/voices/de_DE-thorsten-medium.onnx"
+VOICE_PATH = "/home/tim/projects/piper-voice-api/piper-tts/voices/de_DE-thorsten-medium.onnx"
 
+AUDIO_PATH = "./out/"
 
-# ---------------- Main socket loop ----------------
-def main():
-    voice = PiperVoice.load(VOICE_PATH)
+VOICE_CACHE: dict[str, PiperVoice] = {}
 
-    syn_config = SynthesisConfig(
-        length_scale=1.0,
-    )
+def get_voice(model_path: str) -> PiperVoice:
+    if model_path not in VOICE_CACHE:
+        VOICE_CACHE[model_path] = PiperVoice.load(model_path)
+    return VOICE_CACHE[model_path]
 
-    with wave.open("out.wav", "wb") as wav:
-        for chunk in voice.synthesize("Hello streaming world", syn_config=syn_config):
+def generate_tts(text: str, filename: str, voice: PiperVoice, syn_config: SynthesisConfig) -> bytes:
+    with wave.open(AUDIO_PATH + filename, "wb") as wav:
+        for chunk in voice.synthesize(text, syn_config=syn_config):
             wav.setnchannels(chunk.sample_channels)
             wav.setsampwidth(chunk.sample_width)
             wav.setframerate(chunk.sample_rate)
 
             wav.writeframes(chunk.audio_int16_bytes)
+
+def main():
+    # Ensure the output directory exists
+    if not os.path.exists(AUDIO_PATH):
+        os.makedirs(AUDIO_PATH)
+
+    # Load the voice model
+    voice = get_voice(VOICE_PATH)
+
+    # Configure synthesis parameters
+    syn_config = SynthesisConfig(
+        length_scale=1.0,
+    )
+
+    # Generate Audio
+    generate_tts("Hallo, ich bin eine Teststimme.", "test.wav", voice, syn_config)
+    
 
 if __name__ == "__main__":
     main()
