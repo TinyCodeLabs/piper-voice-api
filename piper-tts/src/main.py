c81cca256c2d75ea160f397eca2c1c1c9d9767e8
Tim Gabrikowski <tim@gabrikowski.de>
2026-02-05 22:16:12 +0100

    Piper TTS Socket server

--- piper-tts/src/main.py
+++ piper-tts/src/main.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
+import json
 import os
+import socket
+import time
+from pathlib import Path
 import wave
 from piper import PiperVoice, SynthesisConfig
 
-VOICE_PATH = "/home/tim/projects/piper-voice-api/piper-tts/voices/de_DE-thorsten-medium.onnx"
+VOICE_PATH = "/home/tim/projects/piper-voice-api/piper-tts/voices/"
+DEFAULT_VOICE = "de_DE-thorsten-medium"
 
 AUDIO_PATH = "./out/"
+SOCKET_PATH = "/tmp/tts.sock"
 
 VOICE_CACHE: dict[str, PiperVoice] = {}
 
@@ -23,13 +29,20 @@ def generate_tts(text: str, filename: str, voice: PiperVoice, syn_config: Synthe
 
             wav.writeframes(chunk.audio_int16_bytes)
 
-def main():
-    # Ensure the output directory exists
-    if not os.path.exists(AUDIO_PATH):
-        os.makedirs(AUDIO_PATH)
+def handle_request(data: dict) -> dict:
+    text = data.get("text", "")
+    voice = data.get("voice", DEFAULT_VOICE)
+    # speed = float(data.get("speed", DEFAULT_SPEED))
+    filename = data.get("filename", f"output_{int(time.time())}.wav")
 
+    if not text:
+        raise ValueError("Missing 'text'")
+
+    # if "<" in text and ">" in text:
+    #     text = ssml_to_text(text)
+   
     # Load the voice model
-    voice = get_voice(VOICE_PATH)
+    voice = get_voice(VOICE_PATH + voice + ".onnx")
 
     # Configure synthesis parameters
     syn_config = SynthesisConfig(
@@ -37,8 +50,45 @@ def main():
     )
 
     # Generate Audio
-    generate_tts("Hallo, ich bin eine Teststimme.", "test.wav", voice, syn_config)
+    generate_tts(text, filename, voice, syn_config)
+
+    return {"status": "ok", "output": filename}
     
+def safe_send(conn, payload: dict):
+    try:
+        conn.sendall(json.dumps(payload).encode("utf-8"))
+    except (BrokenPipeError, OSError):
+        pass
+
+def main():
+    # Ensure the output directory exists
+    if not os.path.exists(AUDIO_PATH):
+        os.makedirs(AUDIO_PATH)
+
+    # Establish socket connection
+    if os.path.exists(SOCKET_PATH):
+        os.unlink(SOCKET_PATH)
+
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    sock.bind(SOCKET_PATH)
+    sock.listen(5)
+
+    print(f"Piper TTS socket listening on {SOCKET_PATH}")
+
+    while True:
+        conn, _ = sock.accept()
+        try:
+            while True:
+                raw = conn.recv(1024 * 1024)               
+
+                try:
+                    data = json.loads(raw.decode("utf-8"))
+                    result = handle_request(data)
+                    safe_send(conn, result)
+                except Exception as e:
+                    safe_send(conn, {"status": "error", "error": str(e)})
+        finally:
+            conn.close()   
 
 if __name__ == "__main__":
     main()
